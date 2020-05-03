5f5d09f27eb6c7fcc42cec9a6dbef59f9aa74b6a
BadLog1n <54963637+BadLog1n@users.noreply.github.com>
2020-05-03 20:31:43 +0300

    first_commit

---
+++ bin char tree test/formBinCharTreeTest.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace bin_char_test_tree
+{
+    partial class formBinCharTreeTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblSearchResult = new System.Windows.Forms.Label();
+            this.btnTraverseTree = new System.Windows.Forms.Button();
+            this.cmbWords = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(5, 5);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(661, 530);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(563, 543);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 28);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(5, 592);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(420, 22);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(435, 590);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 28);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblSearchResult
+            // 
+            this.lblSearchResult.AutoSize = true;
+            this.lblSearchResult.Location = new System.Drawing.Point(543, 596);
+            this.lblSearchResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSearchResult.Name = "lblSearchResult";
+            this.lblSearchResult.Size = new System.Drawing.Size(90, 17);
+            this.lblSearchResult.TabIndex = 4;
+            this.lblSearchResult.Text = "search result";
+            // 
+            // btnTraverseTree
+            // 
+            this.btnTraverseTree.Location = new System.Drawing.Point(16, 543);
+            this.btnTraverseTree.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTraverseTree.Name = "btnTraverseTree";
+            this.btnTraverseTree.Size = new System.Drawing.Size(100, 28);
+            this.btnTraverseTree.TabIndex = 5;
+            this.btnTraverseTree.Text = "traverse tree";
+            this.btnTraverseTree.UseVisualStyleBackColor = true;
+            this.btnTraverseTree.Click += new System.EventHandler(this.btnTraverseTree_Click);
+            // 
+            // cmbWords
+            // 
+            this.cmbWords.FormattingEnabled = true;
+            this.cmbWords.Location = new System.Drawing.Point(124, 543);
+            this.cmbWords.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbWords.Name = "cmbWords";
+            this.cmbWords.Size = new System.Drawing.Size(409, 24);
+            this.cmbWords.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(673, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 159);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // formBinCharTreeTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 635);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbWords);
+            this.Controls.Add(this.btnTraverseTree);
+            this.Controls.Add(this.lblSearchResult);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtInput);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "formBinCharTreeTest";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.TextBox txtSearch;
+		private System.Windows.Forms.Button btnSearch;
+		private System.Windows.Forms.Label lblSearchResult;
+		private System.Windows.Forms.Button btnTraverseTree;
+		private System.Windows.Forms.ComboBox cmbWords;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ bin char tree test/formBinCharTreeTest.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace bin_char_test_tree
+{
+    public partial class formBinCharTreeTest : Form
+    {
+        classLeaf cRoot;
+        char chrNull = '\0';
+        int index = 1;
+        int irl = 0;
+        int prov = 0;
+        public formBinCharTreeTest()
+        {
+            InitializeComponent();
+            cRoot = new classLeaf(chrNull);
+        }
+
+        /// <summary>
+        /// parses words from text in txtInput textbox into a string array then inserts each word int the data-tree
+        /// </summary>
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            char[] cSplitChar = { '\r', '\n', ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+', ']', '[', '{', '}', ';', ':', '\'', '\"', '|', '\\', '?', '/', '>', '.', '<', ',', '`', '~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            string[] strWords = txtInput.Text.Split(cSplitChar, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string s in strWords)
+                insertWord(ref cRoot, s + chrNull);
+
+        }
+
+        /// <summary>
+        /// searches for a word in the database then prints a user-output result in lblSearchResult label
+        /// </summary>
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int irl = wordInTree(ref cRoot, txtSearch.Text);
+            if (irl > 0)
+            {
+                lblSearchResult.Text = "Found" + Convert.ToString(irl);
+            }
+            else
+            {
+                lblSearchResult.Text = "not found";
+            }
+
+        }
+
+        private void btnTraverseTree_Click(object sender, EventArgs e)
+        {
+            string[] strWords = new string[0];
+            traverseTree(ref cRoot, ref strWords, "");
+            /* for (int i = 1; i < strWords.Length; i++)
+             {
+                 strWords[i] = Convert.ToString(i) + " " + strWords[i];
+             }
+             */
+            cmbWords.DataSource = strWords;
+        }
+
+        /// <summary>
+        /// searches for a word in the database by interating through each char of the input word from left to right
+        /// </summary>
+        /// <param name="cLeaf">reference tree-leaf from which search is begun</param>
+        /// <param name="strWord">word to be searched is left-chopped by one char for each iteration until null string or end of search</param>
+        /// <returns>true if word is found, otherwise false</returns>
+        private int wordInTree(ref classLeaf cLeaf, string strWord)
+        {
+            irl = 0;
+            // test first char of word against input Leaf's char field
+            if (strWord[0] > cLeaf.chr)
+            { // first char belongs below this leaf
+                if (cLeaf.down != null)
+                    return wordInTree(ref cLeaf.down, strWord);
+                else
+                    return irl;
+            }
+            else if (strWord[0] < cLeaf.chr)
+            { // first char belongs aleaf, stop, help.bove this leaf
+                if (cLeaf.up != null)
+                    return wordInTree(ref cLeaf.up, strWord);
+                else
+                    return irl;
+            }
+            else if (strWord[0] == cLeaf.chr)
+            { // first char matches this leaf
+                if ((strWord.Length == 1) && (cLeaf.index > 0)) // last char in input word
+                {
+                    irl = cLeaf.index;
+                    return irl;
+                }
+                else if ((strWord.Length == 1) && (cLeaf.index == 0))
+                    return irl;
+                else // iterate along 'next' leaf for second char
+                    return wordInTree(ref cLeaf.next, strWord.Substring(1));
+
+            }
+
+            MessageBox.Show("this should not happen");
+            return irl;
+        }
+
+
+
+
+        /// <summary>
+        /// inserts word into database by iterating along tree proceeding one char at a time from left to right of input word
+        /// </summary>
+        /// <param name="cLeaf">reference to leaf from which insertino is to begin</param>
+        /// <param name="strWord">word to be inserted</param>
+        private void insertWord(ref classLeaf cLeaf, string strWord)
+        {
+            // test first char of word against this leaf
+            if (strWord[0] > cLeaf.chr)
+            { // first char belongs below this one
+
+                if (cLeaf.down != null)
+                {
+                    if (strWord.Length == 2)
+                    {
+                        classLeaf cIndex = new classLeaf(index);
+                        cLeaf.index = index;
+                        index++;
+                    }
+                    insertWord(ref cLeaf.down, strWord); // iterate through tree with current word
+                }
+                else
+                { // there is no leaf going down -> create new leaf 
+                    classLeaf cDown = new classLeaf(strWord[0]);
+                    // set this leaf's down field to new leaf
+                    cLeaf.down = cDown;
+                    // if there are more letters in word, iterate through tree with current word
+                    if (strWord.Length > 1)
+                        insertWord(ref cDown, strWord);
+                }
+            }
+            else if (strWord[0] < cLeaf.chr)
+            { // first char belongs above this leaf
+                if (cLeaf.up != null)
+                    insertWord(ref cLeaf.up, strWord); // iterate through tree with current word
+                else
+                { // there is no leaf going up -> create new leaf
+                    classLeaf cUp = new classLeaf(strWord[0]);
+                    // set this leaf's up field to new leaf
+                    cLeaf.up = cUp;
+                    // if there are more letters in word, iterate through tree with current word
+                    if (strWord.Length > 1)
+                        insertWord(ref cUp, strWord);
+                    /*   if (strWord.Length == 1)
+                       {
+                           classLeaf cIndex = new classLeaf(index);
+                           cLeaf.index = index;
+                           index++;
+                       } */
+                }
+
+            }
+            else if (strWord[0] == cLeaf.chr)
+            { // first char is identical to this leaf's chr field
+                if (strWord.Length > 1)
+                { // the word insertion is not complete
+                    if (cLeaf.next != null)
+                    {
+
+                        if (strWord.Length == 2)
+                        {
+                            classLeaf cIndex = new classLeaf(index);
+                            cLeaf.index = index;
+                            index++;
+                        }
+                        insertWord(ref cLeaf.next, strWord.Substring(1)); // iterate through tree with chopped word
+                    }
+                    else
+                    { // there is no leaf going next -> create new leaf
+                        classLeaf cNext = new classLeaf(strWord[1]);
+                        // set this leaf's next field to new leaf
+                        cLeaf.next = cNext;
+                        /*if ((prov == 1) && (strWord.Length == 2))
+                        {
+                            classLeaf cIndex = new classLeaf(index);
+                            cLeaf.index = index;
+                            index++;
+                        }
+                        */
+                        if ((strWord.Length == 2) && (prov == 0))
+                        {
+                            classLeaf cIndex = new classLeaf(index);
+                            cLeaf.index = index;
+                            index++;
+                        }
+                        // iterate through tree with current word
+                        insertWord(ref cNext, strWord.Substring(1)); // iterate through tree with chopped word
+
+
+                    }
+                }
+            }
+            //дво, дворник, двор, дворовой
+        }
+
+        void traverseTree(ref classLeaf cLeaf, ref string[] strWords, string strThisWord)
+        {
+            if (cLeaf.up != null)
+                traverseTree(ref cLeaf.up, ref strWords, strThisWord);
+
+            string strExtendedWord = strThisWord + cLeaf.chr.ToString();
+            if (cLeaf.chr == chrNull)
+            {
+                Array.Resize<string>(ref strWords, strWords.Length + 1);
+                strWords[strWords.Length - 1] = strExtendedWord;
+            }
+            else
+                if (cLeaf.next != null)
+                traverseTree(ref cLeaf.next, ref strWords, strExtendedWord);
+
+            if (cLeaf.down != null)
+                traverseTree(ref cLeaf.down, ref strWords, strThisWord);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            index = 1;
+            string[] strWords = new string[0];
+            traverseTree(ref cRoot, ref strWords, "");
+            var list = strWords.Cast<string>().ToList();
+            string rro = txtInput.Text + "\0";
+            string userRem = list.FirstOrDefault(us => us == rro);
+            foreach (string s in strWords)
+                deleteWord(ref cRoot, s);
+            if (userRem != null)
+                list.Remove(userRem);
+            cmbWords.DataSource = list;
+            strWords = new string[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                strWords[i] = list[i];
+            }
+            prov = 1;
+            
+            foreach (string s in strWords)
+            {
+                
+                //    char[] MyChar = { ', '0' };
+                //    string str = s.Trim(MyChar) ;
+                insertWord(ref cRoot, s);
+            }
+            prov = 0;
+        }
+
+        int scc = 0;
+        private void deleteWord(ref classLeaf cLeaf, string strWord)
+        {
+            if (scc != 0)
+            {
+
+                if ((cLeaf.down != null) || (cLeaf.next != null)) 
+                {
+                    if ((cLeaf.down != null) && (cLeaf.index == 0) && (strWord.Length != 0))
+                    {
+                        deleteWord(ref cLeaf.down, strWord.Substring(1));
+                    }
+                    if ((cLeaf.next != null)&&(cLeaf.index == 0) && (strWord.Length != 0))
+                    {
+
+                        deleteWord(ref cLeaf.next, strWord.Substring(1));
+                    }
+                    cLeaf.down = null;
+                    cLeaf.up = null;
+                    cLeaf.next = null;
+                    cLeaf.index = 0;
+                }
+
+            }
+            scc++;
+        }
+    }
+
+    public class classLeaf
+    {
+        public classLeaf down;
+        public classLeaf up;
+        public classLeaf next;
+        public int index;
+        public char chr;
+
+        public classLeaf(char c)
+        {
+            chr = c;
+        }
+        public classLeaf(int i)
+        {
+            index = i;
+        }
+    }
+}
